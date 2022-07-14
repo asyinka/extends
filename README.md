e6e72d8f84a4cd6d8c2400c94759f90deedcd50e
asyinka <asyinka@gmail.com>
2022-07-14 11:29:13 +0100

    added project on the use of super and getters git commit -m "added project on the use of "super" and "getters"
    git commit -m "added project on the use of "super" and "getters

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 This project reflect the use of INHERITANCE.
 
 it makes a child class inherit all the methods and properties from another defined class with the EXTENDS keyword in use.
+
+This project also contains the use of SUPER keyword in the constructor scenario.
+It also contains the use of GETTER.
+Take note of the underscore in calling the GET function. The use of "_" renders a value protected and cannot be edited or reassigned without going back to the first declaration.
--- index.js
+++ index.js
@@ -1,44 +1,90 @@
 
 class ability{
 
+    constructor (colour, weight, origin){
+        this.colour = colour;
+        this.weight = weight;
+        this.origin = origin;
+    };
+
     alive = true;
 
     run(){
         console.log(`This ${this.name} can run`)
-    }
+    };
     sleep(){
         console.log(`This ${this.name} is sleeping`)
-    }
+    };
+
+
 }
 
 class rabbit extends ability{
-
     
-    name = "rabbit";
+    constructor(colour, weight, origin, hair){
+        super (colour, weight, origin);
+        this.hair = hair;
+    }
 
+    name = "rabbit";
     hairy(){
         console.log(`This ${this.name} has hairs`)
-    }
+    };
     
 };
 
 class duck extends ability{
 
+    constructor(colour, weight, origin, feathers){
+        super (colour, weight, origin);
+        this.feathers = feathers;
+    }
     name = "duck";
-    run(){
-        console.log(`This ${this.name} can run`)
-    }
-    sleep(){
-        console.log(`This ${this.name} is sleeping`)
-    }
+    
     fly(){
         console.log(`This ${this.name} is flying`)
     }
 };
 
-const duck1 = new duck();
-const rabbit1 = new rabbit();
+const rabbitG = new rabbit("Purple", 15, "Ethopean", "Hairy")
+
+const duck1 = new duck("White", 20, "African", "Fluffy");
+const rabbit1 = new rabbit("White", 20, "African", "hairy");
 
 console.log(duck1.alive)
 duck1.run();
 rabbit1.sleep();
+console.log(duck1.weight);
+console.log(rabbitG.origin)
+console.log(rabbitG.colour);
+console.log(rabbit1.weight)
+
+class goat{
+
+    constructor(hairs, sound, weight){
+        this.hairs = hairs;
+        this.sound = sound;
+        this._weight = weight;
+    }
+    get weight(){
+        return `${this._weight}Kg`
+    } 
+
+    name = "Goat";
+    
+    run(){
+        console.log(`This ${this.name} is running`)
+    }
+};
+
+const Goat = new goat(`hairy`, `bleats`, `29`)
+
+console.log(`The first goat is ${Goat.hairs}`);
+console.log(`The first goat weighs ${Goat.weight}`)
+
+Goat.weight = 35;
+
+console.log(Goat.weight)
+
+const Patience = new goat (`hairy`, `bleats`, 22);
+console.log(Patience.weight)
